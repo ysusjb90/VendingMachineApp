ab10e514091e22991fb8a55f60c846ccceb2b597
Rich Seeds <rich@techelevator.com>
2024-06-05 14:14:58 -0400

    Initial Build

---
+++ src/test/java/com/techelevator/UnitTest1.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UnitTest1 {
+
+
+	@Test
+	public void testMethod1() {
+	}
+}
