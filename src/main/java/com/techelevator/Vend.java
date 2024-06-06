7a13b0dacc6f9ee297532ec47b13824aadbc2234
Scott Brand <ysusjb90@gmail.com>
2024-06-06 17:03:08 -0400

    Feed Money method start in Vend class

--- src/main/java/com/techelevator/Vend.java
+++ src/main/java/com/techelevator/Vend.java
@@ -14,12 +14,29 @@ public class Vend {
     //money out
     private final String INVENTORY_LIST = "vendingmachine.csv";
     private final int MAX_SLOT_NUMBER = 5;
+    private double balance = 0;
+    private double changeNeeded = 0;
+    private static final double NICKEL = .05;
+    private static final double DIME = .10;
+    private static final double QUARTER = .25;
 
+    public String getINVENTORY_LIST() {
+        return INVENTORY_LIST;
+    }
+
+    public int getMAX_SLOT_NUMBER() {
+        return MAX_SLOT_NUMBER;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 
     public Vend() {
         Scanner fileScanner = new Scanner(INVENTORY_LIST);
 
     }
+
     public void loadInventoryList(ItemCounter ic) {
         Map<String, Item> result = new HashMap<>();
         try {
@@ -39,28 +56,55 @@ public class Vend {
 
         }
     }
-    public void loadInventory(Vend vend){
-            Map<String, List<Item>> result = new HashMap<>();
-            List<Item> slotNumberList;
-            try{
-                File dataFile = new File(INVENTORY_LIST);
-                try(Scanner fileScanner = new Scanner(dataFile)){
-                    while(fileScanner.hasNext()){
-                        String[] data = fileScanner.nextLine().split("\\|");
-                        String slotNumber = data[0];
-                        int count = MAX_SLOT_NUMBER;
-                        slotNumberList = new ArrayList<>();
-                        for(int i = 0; i < count; i++){
-                            slotNumberList.add(new Item(data[0], data[1], Double.parseDouble(data[3]), data[4]));
-                        }result.put(slotNumber, slotNumberList);
+
+    public void loadInventory(Vend vend) {
+        Map<String, List<Item>> result = new HashMap<>();
+        List<Item> slotNumberList;
+        try {
+            File dataFile = new File(INVENTORY_LIST);
+            try (Scanner fileScanner = new Scanner(dataFile)) {
+                while (fileScanner.hasNext()) {
+                    String[] data = fileScanner.nextLine().split("\\|");
+                    String slotNumber = data[0];
+                    int count = MAX_SLOT_NUMBER;
+                    slotNumberList = new ArrayList<>();
+                    for (int i = 0; i < count; i++) {
+                        slotNumberList.add(new Item(data[0], data[1], Double.parseDouble(data[3]), data[4]));
                     }
+                    result.put(slotNumber, slotNumberList);
                 }
-            }catch (FileNotFoundException e) {
-                throw new RuntimeException(e); //TODO throw error message update logs
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e); //TODO throw error message update logs
+        }
+    }
+
+
+    private void feedMoney(double balance) {
+        balance = 0;
+        Double userInput = 0.00;
+        Scanner userMoney = new Scanner(String.valueOf(userInput));
+        try {
+            userInput = Double.parseDouble(userMoney.nextLine());
+            balance += userInput;
+            System.out.println(balance);
+        } catch (NumberFormatException nfe) {
+            userInput = 0.00;
+        }
+        private void endTransaction ( double balance){
+            balance = changeNeeded;
+            if (changeNeeded % QUARTER >= 0) {
+                balance = balance - changeNeeded;
+                if (changeNeeded % QUARTER < 1 && changeNeeded % QUARTER > 0) {
+                    balance = balance - changeNeeded % QUARTER * QUARTER;
+                }
             }
         }
+    }
+}
 
-} //TODO balance updates and change return
+
+//TODO balance updates and change return
 //public void callItem (Item newItem){
 
 //}
